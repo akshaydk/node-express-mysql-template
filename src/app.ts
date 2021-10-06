0496d8b78fcb147548673c0de874b3604adbf400
Akshay Kumar <dhupamakshay.kumar@gmail.com>
2021-10-06 12:22:29 +0530

    Feat: Add helmet to the application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import './init';
 import express from 'express';
+import helmet from 'helmet';
 import * as mysql from './middlewares/mysql';
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 app.use(mysql.connect);
 
